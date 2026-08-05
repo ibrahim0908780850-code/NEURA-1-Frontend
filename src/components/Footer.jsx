147a23ca2e15f8136e39cd73b0129ae04938beef
ibrahim0908780850-code <ibrahim0908780850@gmail.com>
2026-08-05 23:17:33 +0200

    Create src/components/Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+export default function Footer(){
+
+return (
+
+<footer className="
+border-t
+border-white/10
+bg-slate-950
+py-8
+text-center
+text-gray-500
+">
+
+
+<p>
+© 2026 NEURA-1
+</p>
+
+<p className="text-sm mt-2">
+Arabic-first AI Cloud System
+</p>
+
+
+</footer>
+
+)
+
+}
