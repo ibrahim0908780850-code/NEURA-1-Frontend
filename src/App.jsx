18f6e7b7e7a46d19ad931f4f6c04f3a11c3fa696
ibrahim0908780850-code <ibrahim0908780850@gmail.com>
2026-08-05 23:15:55 +0200

    Create src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
+
+
+export default function App(){
+
+return (
+
+<BrowserRouter>
+
+<Routes>
+
+
+<Route
+path="/"
+element={<Home />}
+/>
+
+
+<Route
+path="/chat"
+element={<Chat />}
+/>
+
+
+</Routes>
+
+</BrowserRouter>
+
+)
+
+}
